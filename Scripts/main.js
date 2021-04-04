8fd10af6341e7d0d23b8782db16ca742903cdcd4
Restyled.io <commits@restyled.io>
2021-04-04 16:36:01 +0000

    Restyled by clang-format

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,47 +1,45 @@
 // Created a pfp object to be used
-function createPfpObject(type = "MESSAGE", userID = undefined, UserName = undefined)
-{
-    var profilePicObject = document.createElement("img");  
-    if(type == "MESSAGE") {
-        profilePicObject.classList.add("messagePfp");	// ID For css
-    } else if(type == "GUILD") {
-        profilePicObject.classList.add("guildPfp");	// ID For css
-    }
-    profilePicObject.setAttribute("src", "../../Assets/pfp.png");           // place holder id backend team replace
-    return profilePicObject;
+function createPfpObject(type = "MESSAGE", userID = undefined,
+                         UserName = undefined) {
+  var profilePicObject = document.createElement("img");
+  if (type == "MESSAGE") {
+    profilePicObject.classList.add("messagePfp"); // ID For css
+  } else if (type == "GUILD") {
+    profilePicObject.classList.add("guildPfp"); // ID For css
+  }
+  profilePicObject.setAttribute(
+      "src", "../../Assets/pfp.png"); // place holder id backend team replace
+  return profilePicObject;
 }
 //#region pfp wrappers
 // Created a guild pfp object to be used
 function createGuildPfpObject(userID = undefined, UserName = undefined) {
-    return createPfpObject("GUILD", userID, UserName);
+  return createPfpObject("GUILD", userID, UserName);
 }
 // Created a guild pfp object to be used
 function createMessagePfpObject(userID = undefined, UserName = undefined) {
-    return createPfpObject("MESSAGE", userID, UserName);
+  return createPfpObject("MESSAGE", userID, UserName);
 }
 //#endregion
 
 // Testing function.
 function test() {
-    console.log(`Running test!`);
+  console.log(`Running test!`);
 
-    var localUserState = localCache.localData; 
-    
-    localCache = { 
-        localUserState, 
-        guilds: []
-    };
+  var localUserState = localCache.localData;
 
-    clear_screen();
+  localCache = {localUserState, guilds : []};
 
-    // Begin to re-populate guild cache
-    addGuildToCache(1, "Guild Name.", "Descriptionnn");
-    // Begin to re-populate guild cache
-    addGuildToCache(2, "Guild Name.", "Descriptionnn");
+  clear_screen();
 
-    // Attempt to add 2 guilds of the same id.
-    addGuildToCache(2, "Guild Name.", "Descriptionnn");
+  // Begin to re-populate guild cache
+  addGuildToCache(1, "Guild Name.", "Descriptionnn");
+  // Begin to re-populate guild cache
+  addGuildToCache(2, "Guild Name.", "Descriptionnn");
 
-    renderGuildsBar();
+  // Attempt to add 2 guilds of the same id.
+  addGuildToCache(2, "Guild Name.", "Descriptionnn");
+
+  renderGuildsBar();
 }
 log("Loaded. main.js")
--- Scripts/rendering_controller.js
+++ Scripts/rendering_controller.js
@@ -1,8 +1,8 @@
 function renderGuildsBar() {
-    localCache.guilds.forEach(guild => {
-        log(`Rendering guild with id: ${guild.id}`);
-        // Call low level function for rendering a new guild.
-        displayGuild_Localy(guild.id);
-    })
+  localCache.guilds.forEach(guild => {
+    log(`Rendering guild with id: ${guild.id}`);
+    // Call low level function for rendering a new guild.
+    displayGuild_Localy(guild.id);
+  })
 }
 log("Loaded. rendering_controller.js")
